0e2e4fe05fe9b231810a3ae18a6bc9156c1e1b07
AOLink <aolink101@gmail.com>
2020-12-01 18:47:22 -0500

    created projectlist in the Dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Notifications from './Notifications'
+import ProjectList from '../projects/ProjectList'
 
 class Dashboard extends Component {
     render() {
@@ -6,8 +8,14 @@ class Dashboard extends Component {
         return (
             <div className="dashboard container">
                 <div className="row">
-                    <div className="col s12 m6"></div>
-                    <div className="col s12 m5 offset-m1"></div>
+                    <div className="col s12 m6">
+                        <ProjectList />
+                    </div>
+                        
+                    <div className="col s12 m5 offset-m1">
+                    <Notifications />
+
+                    </div>
                 </div>
             </div>
 
@@ -15,3 +23,5 @@ class Dashboard extends Component {
     }
 
 }
+
+export default Dashboard
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,34 @@
+//functional component
+
+import React from 'react'
+
+const ProjectList = () => {
+    return(
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <di className="car-content grey-text text-darken-3">
+                    <span className="card-title">Proejct Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text">27 August, 2020</p>
+                </di>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <di className="car-content grey-text text-darken-3">
+                    <span className="card-title">Proejct Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text">27 August, 2020</p>
+                </di>
+            </div>
+            <div className="card z-depth-0 project-summary">
+                <di className="car-content grey-text text-darken-3">
+                    <span className="card-title">Proejct Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text">27 August, 2020</p>
+                </di>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList
