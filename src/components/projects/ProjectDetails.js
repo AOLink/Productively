a6675ad46846e02abb27d7c04513b6327b0d350f
AOLink <aolink101@gmail.com>
2020-12-01 21:11:20 -0500

    built out project details page

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+    const id = props.match.params.id;
+    console.log(props)
     return (
         <div className="container section project-details">
             <div className="card z-depth-0">
                 <div className="card-content">
-                    <span className="card-title">Title</span>
+                    <span className="card-title">Title - {id} </span>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, aliquam! Nisi, quam sed enim, rem, ipsum nostrum veritatis voluptatum omnis aliquid aliquam aut quas a atque labore repellendus et quibusdam?</p>
                 </div>
-                <div className="card-action green lighten-4 green-text">
+                <div className="card-action gret lighten-4 grey-text">
                     <div>Posted by Me</div>
                     <div>27th August, 5am</div>
                 </div>
