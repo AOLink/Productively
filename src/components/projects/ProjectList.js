36be0112c15791f7e2fce29edfc7c738695dea8f
AOLink <aolink101@gmail.com>
2020-12-01 19:36:37 -0500

    created summary component

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,32 +1,16 @@
 //functional component
 
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return(
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <di className="car-content grey-text text-darken-3">
-                    <span className="card-title">Proejct Title</span>
-                    <p> Posted by Me</p>
-                    <p className="grey-text">27 August, 2020</p>
-                </di>
-            </div>
+           <ProjectSummary />
+           <ProjectSummary />
+           <ProjectSummary />
+           <ProjectSummary />
 
-            <div className="card z-depth-0 project-summary">
-                <di className="car-content grey-text text-darken-3">
-                    <span className="card-title">Proejct Title</span>
-                    <p> Posted by Me</p>
-                    <p className="grey-text">27 August, 2020</p>
-                </di>
-            </div>
-            <div className="card z-depth-0 project-summary">
-                <di className="car-content grey-text text-darken-3">
-                    <span className="card-title">Proejct Title</span>
-                    <p> Posted by Me</p>
-                    <p className="grey-text">27 August, 2020</p>
-                </di>
-            </div>
         </div>
     )
 }
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+    return(
+        <div className="card z-depth-0 project-summary">
+                <div className="car-content grey-text text-darken-3">
+                    <span className="card-title">Proejct Title</span>
+                    <p> Posted by Me</p>
+                    <p className="grey-text">27 August, 2020</p>
+                </div> 
+            </div>
+    )
+}
+
+export default ProjectSummary
