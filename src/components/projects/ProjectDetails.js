a3ffd9baf3fca0a76098aa0e59200b523ad487bd
AOLink <aolink101@gmail.com>
2020-12-01 20:47:41 -0500

    Issues rendering Project Details Page :/

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Title</span>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, aliquam! Nisi, quam sed enim, rem, ipsum nostrum veritatis voluptatum omnis aliquid aliquam aut quas a atque labore repellendus et quibusdam?</p>
+                </div>
+                <div className="card-action green lighten-4 green-text">
+                    <div>Posted by Me</div>
+                    <div>27th August, 5am</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -4,7 +4,7 @@ const ProjectSummary = () => {
     return(
         <div className="card z-depth-0 project-summary">
                 <div className="car-content grey-text text-darken-3">
-                    <span className="card-title">Proejct Title</span>
+                    <span className="card-title">Project Title</span>
                     <p> Posted by Me</p>
                     <p className="grey-text">27 August, 2020</p>
                 </div> 
