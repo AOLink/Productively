b083402d2ef3acdfb361280d3d075ce915dd5e77
AOLink <aolink101@gmail.com>
2020-12-03 17:10:28 -0500

    setup  thunk -  dispatch

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import root from './store/reducers/root'
 import { Provider } from 'react-redux'
-const store = createStore(root);
+import thunk from 'redux-thunk'
+
+
+const store = createStore(root, applyMiddleware(thunk) );
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode> 
    <Provider store={store}><App /></Provider> 
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/reducers/project.js
+++ src/store/reducers/project.js
@@ -7,7 +7,11 @@ const initState = {
  }
 
 const projectReducer = (state = initState, action) => {
+        switch (action.type) {
+                case 'CREATE_PROJECT':
+                  console.log('CREATED PROJECT', action.project)
 
+        }
             return state
 }
 
