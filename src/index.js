c8a2859a025998977485b614b2b97790818cbc87
AOLink <aolink101@gmail.com>
2020-12-04 02:26:29 -0500

    Firebase & Firestore

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import root from './store/reducers/root'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
 
+const store = createStore(root, 
+  
+  compose(
+  applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore })),
+  reduxFirestore(),
+  reactReduxFirebase()
 
-const store = createStore(root, applyMiddleware(thunk) );
+  )
+  );
 
 
 ReactDOM.render(
