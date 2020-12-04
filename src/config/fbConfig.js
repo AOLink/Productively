5bce265e92b7d4e90b31a27b2671de40dd887524
AOLink <aolink101@gmail.com>
2020-12-04 01:46:13 -0500

    Initialize firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyALlButvHr1NUojYtTCwYVBmLoyLTq4LT4",
+    authDomain: "productively-ffc5f.firebaseapp.com",
+    projectId: "productively-ffc5f",
+    storageBucket: "productively-ffc5f.appspot.com",
+    messagingSenderId: "814181125798",
+    appId: "1:814181125798:web:608f7238045c963eb92e67"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({timestampsInSnapshots: true})
+  
+  export default firebase;
